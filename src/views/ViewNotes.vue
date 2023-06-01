9f4022c48d5e1dd8ae048716bc0a34e2e20fc1e4
Xander Dyer <xdyer@confirmedapp.com>
2023-05-31 23:58:13 -0400

    style and design changes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="notes">
-        <h1>Notes</h1>
+  <div class="notes">
+
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="TAdd a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New Note</button>
+        </div>
+      </div>
     </div>
+
+    <div class="card mb-4" v-for="i in 3">
+      <p class="card-header-title">Component</p>
+      <button class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </button>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+          corporis suscipit voluptas earum, a nostrum expedita ullam quibusdam
+          repudiandae voluptatem molestias error optio fugit quasi dignissimos
+          totam quidem, enim neque.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Save</a>
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
